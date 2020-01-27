88385a53dbf27eb8ad7da216856837f95c9928ce
Ivan <ivanprivalov@mail.ru>
2020-01-28 02:19:17 +0300

    Code improvements

--- js/stat.js
+++ js/stat.js
@@ -5,32 +5,26 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 
-var GAP = 10;
-var COLUMN_GAP = 50;
-var PADDING = 30;
-
 var TEXT_HEIGHT = 16;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 
-var textField = PADDING + GAP + TEXT_HEIGHT * 2; // the variable is used to calculate the vertical space used by the 2 first lines of the text
+var GAP = 10;
+var COLUMN_GAP = 50;
+var PADDING = 30;
 
 var columnWidth = BAR_WIDTH + COLUMN_GAP; // the variable is used to divide the cloud into vertical columns
 
-var startingPositions = { // the object is used to identify starting positions for objects inside of the cloud
+var startingPositions = {// the object is used to identify starting positions for objects inside of the cloud
   horizontal: CLOUD_X + PADDING,
   vertical: CLOUD_Y + PADDING
 };
 
-var idents = {
-  secondTextLine: startingPositions.vertical + TEXT_HEIGHT + GAP,
-  result: this.secondTextLine + GAP,
-  bar: this.result + TEXT_HEIGHT + GAP,
-  name: this.bar + BAR_HEIGHT + GAP
-};
-// debugger;
-console.log(idents.secondTextLine);
-console.log(idents.result);
+var secondTextLineIdent = startingPositions.vertical + TEXT_HEIGHT + GAP;
+var resultIdent = secondTextLineIdent + TEXT_HEIGHT + GAP;
+var barIdent = resultIdent + GAP;
+var nameIdent = barIdent + BAR_HEIGHT + GAP * 2;
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -49,8 +43,8 @@ var getMaxElement = function (arr) {
 };
 
 var getBarPosition = function (times, index, maxTime) { // the variable is used to the position of baseline of the bar
-  BAR_HEIGHT - BAR_HEIGHT * times[index] / maxTime;
- }
+  return BAR_HEIGHT - BAR_HEIGHT * times[index] / maxTime;
+};
 
 var getColor = function (players, index) {
   if (players[index] === 'Вы') { // for the 'Вы' player the color is predefined
@@ -68,19 +62,19 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillStyle = '#000';
   ctx.font = TEXT_HEIGHT + 'px "PT Mono"';
   ctx.fillText('Ура вы победили!', startingPositions.horizontal, startingPositions.vertical);
-  ctx.fillText('Вот список результатов:', startingPositions.horizontal, idents.secondTextLine);
+  ctx.fillText('Вот список результатов:', startingPositions.horizontal, secondTextLineIdent);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
 
     ctx.fillStyle = 'black';
-    ctx.fillText(Math.floor(times[i]), startingPositions.horizontal + columnWidth * i, idents.result);
+    ctx.fillText(Math.floor(times[i]), startingPositions.horizontal + columnWidth * i, resultIdent);
 
     ctx.fillStyle = getColor(players, i);
-    ctx.fillRect(startingPositions.horizontal + columnWidth * i, idents.bar + getBarPosition(times, i, maxTime), BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(startingPositions.horizontal + columnWidth * i, barIdent + getBarPosition(times, i, maxTime), BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
 
     ctx.fillStyle = 'black';
-    ctx.fillText(players[i], startingPositions.horizontal + columnWidth * i, idents.name);
+    ctx.fillText(players[i], startingPositions.horizontal + columnWidth * i, nameIdent);
   }
 };
